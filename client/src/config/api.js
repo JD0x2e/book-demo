2dff5d4695ccd5af7c70a8e11fa81aaeb9c3b40d
Jack <jackdoy99@icloud.com>
2022-11-24 13:25:30 +0000

    config file

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,4 +1,4 @@
-const LIVE_API_URL = "https://coinswapper-api.netlify.app/.netlify/functions/api";
+const LIVE_API_URL = "https://libraryofbooks-api.netlify.app/.netlify/functions/api";
 // const DEV_API_URL = "http://localhost:9000/.netlify/functions/api";
 
 export const API_URL = LIVE_API_URL;
